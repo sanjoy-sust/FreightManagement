31b4dc5875c124b5456b0de44f0add88cea42d94
Sanjoy KUmer <sanjoyd.cse@gmail.com>
2017-11-03 10:06:03 +0600

    Cache implemented

--- src/main/java/com/fm/assignment/config/AppConfig.java
+++ src/main/java/com/fm/assignment/config/AppConfig.java
@@ -3,9 +3,15 @@ package com.fm.assignment.config;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.omg.CORBA.UNKNOWN;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
 
 /**
  * @author Sanjoy Kumer Deb
@@ -13,6 +19,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @EnableWebMvc
 @Configuration
+@EnableCaching
 public class AppConfig {
     /**
      * This bean is customised to allow Quoted fields as field data.
@@ -25,5 +32,12 @@ public class AppConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         return objectMapper;
+    }
+    @Bean
+    CacheManager cacheManager() {
+
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("findLocationWithin")));
+        return cacheManager;
     }
 }

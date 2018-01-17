d9d1da91d1a3902252156dd17566626c7704e4b2
Sanjoy Kumer Deb <sanjoyd.cse@gmail.com>
2018-01-18 00:45:26 +0600

    [ref #4] - Default schema change

--- src/main/java/com/fm/assignment/tenant/TenantContext.java
+++ src/main/java/com/fm/assignment/tenant/TenantContext.java
@@ -4,7 +4,7 @@ package com.fm.assignment.tenant;
  * Created by Lenovo on 18/01/2018.
  */
 public class TenantContext {
-    final public static String DEFAULT_TENANT = "flopcoder";
+    final public static String DEFAULT_TENANT = "freight_management";
 
     private static ThreadLocal<String> currentTenant = new ThreadLocal<String>()
     {

853bd75c0fbbaae868eb87bf86735c69d90c9ba2
Sanjoy Kumer Deb <sanjoyd.cse@gmail.com>
2018-01-22 00:19:16 +0600

    [refs #1] - dynamic property loaded

--- src/test/java/com/fm/assignment/repository/PlaceRepositoryTest.java
+++ src/test/java/com/fm/assignment/repository/PlaceRepositoryTest.java
@@ -4,14 +4,17 @@ import com.fm.assignment.core.dao.PathRepository;
 import com.fm.assignment.core.dao.PlaceRepository;
 import com.fm.assignment.core.entity.PathEntity;
 import com.fm.assignment.core.entity.PlaceEntity;
+import com.fm.assignment.repository.constants.PlaceEntityConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.NonUniqueResultException;
 import java.util.List;
 
 /**
@@ -33,31 +36,25 @@ public class PlaceRepositoryTest {
         Assert.assertNull(placeRepository.findOne(id));
     }
 
-    @Test(expected = Exception.class)
-    public void addEmptyObj() {
-        PlaceEntity placeEntity = new PlaceEntity();
-        placeRepository.save(placeEntity);
-    }
-
     @Test
     public void findPathByName() {
         PlaceEntity placeEntity = savePlaceData();
-        Assert.assertEquals(placeEntity.getName(), "DD");
+        Assert.assertEquals(placeEntity.getName(), PlaceEntityConstants.PLACE_NAME);
+        placeRepository.delete(placeEntity);
     }
 
     @Test
     public void findPathByVal() {
-        PlaceEntity pe = savePlaceData();
-        PlaceEntity save = placeRepository.save(pe);
-        PlaceEntity placeEntity = placeRepository.findByCode("CMI");
-        Assert.assertEquals(placeEntity.getName(), "DD");
+        PlaceEntity save = savePlaceData();
+        PlaceEntity placeEntity = placeRepository.findByCode(PlaceEntityConstants.PLACE_CODE);
+        Assert.assertEquals(placeEntity.getName(), PlaceEntityConstants.PLACE_NAME);
         placeRepository.delete(save);
     }
 
     @Test
      public void findPathByValNotExits() {
         PlaceEntity placeEntity = null;
-        placeEntity = placeRepository.findByCode("CMI");
+        placeEntity = placeRepository.findByCode(PlaceEntityConstants.PLACE_CODE);
         if(placeEntity != null)
         {
             placeRepository.delete(placeEntity);
@@ -65,23 +62,35 @@ public class PlaceRepositoryTest {
             PlaceEntity save = placeRepository.save(pe);
             placeRepository.delete(save);
         }
-        placeEntity = placeRepository.findByCode("CMI");
+        placeEntity = placeRepository.findByCode(PlaceEntityConstants.PLACE_CODE);
         Assert.assertNull(placeEntity);
     }
 
     @Test(expected = NullPointerException.class)
     public void findPathByValNullPointer() {
-        PlaceEntity placeEntity = placeRepository.findByCode("CMI");
-        String name = placeEntity.getName();
-        log.error(name);
+        PlaceEntity placeEntity = placeRepository.findByCode(PlaceEntityConstants.PLACE_CODE);
+        placeEntity.getName();
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void checkDuplicateEntry() {
+        savePlaceData();
+        PlaceEntity placeEntity = placeRepository.findByCode(PlaceEntityConstants.PLACE_CODE);
+        Assert.assertNotNull(placeEntity.getName());
+        try {
+            savePlaceData();
+        }finally {
+            placeRepository.delete(placeEntity);
+        }
     }
 
     private PlaceEntity savePlaceData() {
         PlaceEntity pe = new PlaceEntity();
-        pe.setName("DD");
-        pe.setCode("CMI");
-        pe.setLatitude(23.894929);
-        pe.setLongitude(90.868706);
+        pe.setName(PlaceEntityConstants.PLACE_NAME);
+        pe.setCode(PlaceEntityConstants.PLACE_CODE);
+        pe.setLatitude(PlaceEntityConstants.PLACE_LATITUDE);
+        pe.setLongitude(PlaceEntityConstants.PLACE_LONGITUDE);
+        pe = placeRepository.save(pe);
         return pe;
     }
 }
---
+++ src/test/java/com/fm/assignment/repository/constants/PlaceEntityConstants.java
@@ -0,0 +1,11 @@
+package com.fm.assignment.repository.constants;
+
+/**
+ * Created by Lenovo on 21/01/2018.
+ */
+public class PlaceEntityConstants {
+    public static final String PLACE_NAME = "DD";
+    public static final String PLACE_CODE = "CMI";
+    public static final Double PLACE_LATITUDE= 23.894929;
+    public static final Double PLACE_LONGITUDE = 90.868706;
+}
